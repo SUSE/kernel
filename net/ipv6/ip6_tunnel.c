a4c8192dd5cf1e9a826a975dcd470be216111329
Michal Kubecek <mkubecek@suse.cz>
2026-01-22 00:55:03 +0100

    ipv6: adopt dst_dev() helper (CVE-2025-40139 bsc#1253409).
    suse-commit: 94105f67107279ed01d21ebc3f86d12f1126ed9b

--- net/ipv6/ip6_tunnel.c
+++ net/ipv6/ip6_tunnel.c
@@ -1124,7 +1124,7 @@ route_lookup:
 		ndst = dst;
 	}
 
-	tdev = dst->dev;
+	tdev = dst_dev(dst);
 
 	if (tdev == dev) {
 		stats->collisions++;
@@ -1200,7 +1200,7 @@ route_lookup:
 	/* Calculate max headroom for all the headers and adjust
 	 * needed_headroom if necessary.
 	 */
-	max_headroom = LL_RESERVED_SPACE(dst->dev) + sizeof(struct ipv6hdr)
+	max_headroom = LL_RESERVED_SPACE(tdev) + sizeof(struct ipv6hdr)
 			+ dst->header_len + t->hlen;
 	if (max_headroom > dev->needed_headroom)
 		dev->needed_headroom = max_headroom;
