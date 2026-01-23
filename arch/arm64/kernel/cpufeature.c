58dc749d52eb3cc17630b5dc7b05de1db1bb8ce9
Ivan T. Ivanov <iivanov@suse.de>
2026-01-23 16:43:56 +0200

    arm64: cpufeature: add Neoverse-V3AE to BBML2 allow list (jsc#PED-13757)
    suse-commit: e6fb7e8e97e17ad612619b87a0d8315f72eb79f9

--- arch/arm64/kernel/cpufeature.c
+++ arch/arm64/kernel/cpufeature.c
@@ -2186,6 +2186,7 @@ bool cpu_supports_bbml2_noabort(void)
 	static const struct midr_range supports_bbml2_noabort_list[] = {
 		MIDR_REV_RANGE(MIDR_CORTEX_X4, 0, 3, 0xf),
 		MIDR_REV_RANGE(MIDR_NEOVERSE_V3, 0, 2, 0xf),
+		MIDR_REV_RANGE(MIDR_NEOVERSE_V3AE, 0, 2, 0xf),
 		MIDR_ALL_VERSIONS(MIDR_NVIDIA_OLYMPUS),
 		MIDR_ALL_VERSIONS(MIDR_AMPERE1),
 		MIDR_ALL_VERSIONS(MIDR_AMPERE1A),
