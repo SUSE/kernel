4e2c78e7514d4c8df5082a5fa0153e0dd6647319
Chun-Yi Lee <jlee@suse.com>
2026-01-29 13:58:10 +0800

    ACPI: MRRM: Fix default max memory region (jsc#PED-14260).
    suse-commit: d540a6128d0cab43dce89972cbd1418ebe5d8472

--- include/linux/acpi.h
+++ include/linux/acpi.h
@@ -1097,7 +1097,7 @@ static inline acpi_handle acpi_get_processor_handle(int cpu)
 
 static inline int acpi_mrrm_max_mem_region(void)
 {
-	return -ENOENT;
+	return 1;
 }
 
 #endif	/* !CONFIG_ACPI */
