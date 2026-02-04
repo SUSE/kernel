900e444888ab8bb88b749c220ad85ff80c40c066
Thomas Bogendoerfer <tbogendoerfer@suse.de>
2026-02-04 16:37:06 +0100

    net/mlx5: HWS, prevent rehash from filling up the queues (git-fixes).
    suse-commit: 72f293ddec04caf6e6e12e07866fdb5ac6cf4929

--- drivers/net/ethernet/mellanox/mlx5/core/steering/hws/bwc.c
+++ drivers/net/ethernet/mellanox/mlx5/core/steering/hws/bwc.c
@@ -620,6 +620,7 @@ static int hws_bwc_matcher_move_all_simple(struct mlx5hws_bwc_matcher *bwc_match
 	struct list_head *rules_list;
 	u32 pending_rules;
 	int i, ret = 0;
+	bool drain;
 
 	mlx5hws_bwc_rule_fill_attr(bwc_matcher, 0, 0, &rule_attr);
 
@@ -647,10 +648,12 @@ static int hws_bwc_matcher_move_all_simple(struct mlx5hws_bwc_matcher *bwc_match
 			}
 
 			pending_rules++;
+			drain = pending_rules >=
+				hws_bwc_get_burst_th(ctx, rule_attr.queue_id);
 			ret = mlx5hws_bwc_queue_poll(ctx,
 						     rule_attr.queue_id,
 						     &pending_rules,
-						     false);
+						     drain);
 			if (unlikely(ret)) {
 				if (ret == -ETIMEDOUT) {
 					mlx5hws_err(ctx,
