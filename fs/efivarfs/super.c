0ff3e7b03fa5a1d1780adcb1d7b6f19639b235ec
Chun-Yi Lee <jlee@suse.com>
2026-02-07 15:35:43 +0800

    efivarfs: remove unused efi_variable.Attributes and efivar_entry.kobj (jsc#PED-14259).
    suse-commit: 7b851d77736a751db28a03b61aedd1e951522296

--- fs/efivarfs/super.c
+++ fs/efivarfs/super.c
@@ -249,7 +249,7 @@ static int efivarfs_callback(efi_char16_t *name16, efi_guid_t vendor,
 
 	inode_lock(inode);
 	inode->i_private = entry;
-	i_size_write(inode, size + sizeof(entry->var.Attributes));
+	i_size_write(inode, size + sizeof(__u32)); /* attributes + data */
 	inode_unlock(inode);
 	d_add(dentry, inode);
 
