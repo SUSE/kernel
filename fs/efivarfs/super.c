b82e2b9d1acd53362b73082aa69320b87dbec8c8
Chun-Yi Lee <jlee@suse.com>
2026-02-07 15:35:43 +0800

    efivarfs: Defer PM notifier registration until .fill_super (jsc#PED-14259).
    suse-commit: 269ecb7b90663fbc144b044d92fb38a8a695af68

--- fs/efivarfs/super.c
+++ fs/efivarfs/super.c
@@ -371,6 +371,8 @@ static int efivarfs_fill_super(struct super_block *sb, struct fs_context *fc)
 	if (err)
 		return err;
 
+	register_pm_notifier(&sfi->pm_nb);
+
 	return efivar_init(efivarfs_callback, sb, true);
 }
 
@@ -562,7 +564,6 @@ static int efivarfs_init_fs_context(struct fs_context *fc)
 
 	sfi->pm_nb.notifier_call = efivarfs_pm_notify;
 	sfi->pm_nb.priority = 0;
-	register_pm_notifier(&sfi->pm_nb);
 
 	return 0;
 }
