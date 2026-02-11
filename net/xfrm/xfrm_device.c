11d95430b7f79c72196b1e559dced45100ec9306
Thomas Bogendoerfer <tbogendoerfer@suse.de>
2026-02-11 11:00:37 +0100

    xfrm: Check inner packet family directly from skb_dst (git-fixes).
    suse-commit: b32d6d41b84c74fa55f78d4bc7cbc0106234732f

--- net/xfrm/xfrm_device.c
+++ net/xfrm/xfrm_device.c
@@ -435,7 +435,7 @@ ok:
 
 	check_tunnel_size = x->xso.type == XFRM_DEV_OFFLOAD_PACKET &&
 			    x->props.mode == XFRM_MODE_TUNNEL;
-	switch (x->inner_mode.family) {
+	switch (skb_dst(skb)->ops->family) {
 	case AF_INET:
 		/* Check for IPv4 options */
 		if (ip_hdr(skb)->ihl != 5)
