2e8e25aaf6ea3e635a7baa4eced84dcd611d2c7d
Patrik Jakobsson <pjakobsson@suse.de>
2026-02-12 17:05:48 +0100

    drm/amdgpu: update the handle ptr in soft_reset (jsc#PED-13979 jsc#PED-14039 jsc#PED-14046 jsc#PED-14211 jsc#PED-14333 jsc#PED-14487 jsc#PED-14488 jsc#PED-14497 jsc#PED-14499)
    suse-commit: 50d85040526147fe06c6359a36edb48b21617048

--- drivers/gpu/drm/amd/pm/legacy-dpm/kv_dpm.c
+++ drivers/gpu/drm/amd/pm/legacy-dpm/kv_dpm.c
@@ -3118,7 +3118,7 @@ static int kv_dpm_wait_for_idle(void *handle)
 }
 
 
-static int kv_dpm_soft_reset(void *handle)
+static int kv_dpm_soft_reset(struct amdgpu_ip_block *ip_block)
 {
 	return 0;
 }
