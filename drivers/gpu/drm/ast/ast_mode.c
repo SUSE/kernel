6d74dac1b244c08017878837040b03265c31f41d
Patrik Jakobsson <pjakobsson@suse.de>
2026-02-12 15:46:26 +0100

    drm/ast: Respect return value from CRTC init (jsc#PED-13979 jsc#PED-14039 jsc#PED-14046 jsc#PED-14211 jsc#PED-14333 jsc#PED-14487 jsc#PED-14488 jsc#PED-14497 jsc#PED-14499)
    suse-commit: b42555e14dadf3f863cdc98aa74e2772a7c343af

--- drivers/gpu/drm/ast/ast_mode.c
+++ drivers/gpu/drm/ast/ast_mode.c
@@ -1400,7 +1400,9 @@ int ast_mode_config_init(struct ast_device *ast)
 	if (ret)
 		return ret;
 
-	ast_crtc_init(dev);
+	ret = ast_crtc_init(dev);
+	if (ret)
+		return ret;
 
 	switch (ast->tx_chip) {
 	case AST_TX_NONE:
