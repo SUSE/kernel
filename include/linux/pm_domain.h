e3978a02523de08dedd5bdce21e24853255a03b0
Patrik Jakobsson <pjakobsson@suse.de>
2026-02-12 15:40:59 +0100

    - pmdomain: core: Set the required dev for a required OPP during genpd (jsc#PED-13979 jsc#PED-14039 jsc#PED-14046 jsc#PED-14211 jsc#PED-14333 jsc#PED-14487 jsc#PED-14488 jsc#PED-14497 jsc#PED-14499) - Refresh patches.suse/pmdomain-core-Introduce-dev_pm_genpd_rpm_always_on.patch
    suse-commit: 4870cc87e440edfb4076c7c49a7732142d9a65de

--- include/linux/pm_domain.h
+++ include/linux/pm_domain.h
@@ -258,6 +258,7 @@ struct generic_pm_domain_data {
 	unsigned int performance_state;
 	unsigned int default_pstate;
 	unsigned int rpm_pstate;
+	unsigned int opp_token;
 	bool hw_mode;
 	bool rpm_always_on;
 	void *data;
