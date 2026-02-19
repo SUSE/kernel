e5f4ce6edf14daf3482ae2dfe8b4422dedaab922
Patrik Jakobsson <pjakobsson@suse.de>
2026-02-19 17:30:40 +0100

    drm/i915/psr: Disable Panel Replay as well if VRR is enabled (jsc#PED-13979 jsc#PED-14039 jsc#PED-14046 jsc#PED-14211 jsc#PED-14333 jsc#PED-14487 jsc#PED-14488 jsc#PED-14497 jsc#PED-14499)
    suse-commit: 83b87cfe954cffb999364d8bf6e31a37bff18fd5

--- drivers/gpu/drm/i915/display/intel_psr.c
+++ drivers/gpu/drm/i915/display/intel_psr.c
@@ -1568,13 +1568,6 @@ static bool _psr_compute_config(struct intel_dp *intel_dp,
 	const struct drm_display_mode *adjusted_mode = &crtc_state->hw.adjusted_mode;
 	int entry_setup_frames;
 
-	/*
-	 * Current PSR panels don't work reliably with VRR enabled
-	 * So if VRR is enabled, do not enable PSR.
-	 */
-	if (crtc_state->vrr.enable)
-		return false;
-
 	if (!CAN_PSR(intel_dp))
 		return false;
 
@@ -1682,6 +1675,12 @@ void intel_psr_compute_config(struct intel_dp *intel_dp,
 			    "PSR disabled due to joiner\n");
 		return;
 	}
+
+	/*
+	 * Currently PSR/PR doesn't work reliably with VRR enabled.
+	 */
+	if (crtc_state->vrr.enable)
+		return;
 
 	crtc_state->has_panel_replay = _panel_replay_compute_config(intel_dp,
 								    crtc_state,
