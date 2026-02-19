c2a3e7619d89044f6421b16b959d4c276d1f9437
Patrik Jakobsson <pjakobsson@suse.de>
2026-02-19 17:20:33 +0100

    drm/xe: Avoid evicting object of the same vm in none fault mode (jsc#PED-13979 jsc#PED-14039 jsc#PED-14046 jsc#PED-14211 jsc#PED-14333 jsc#PED-14487 jsc#PED-14488 jsc#PED-14497 jsc#PED-14499)
    suse-commit: 958a4ad716164925acef70c42190001013c0748a

--- drivers/gpu/drm/xe/xe_vm.c
+++ drivers/gpu/drm/xe/xe_vm.c
@@ -2426,18 +2426,18 @@ static void vm_bind_ioctl_ops_unwind(struct xe_vm *vm,
 }
 
 static int vma_lock_and_validate(struct drm_exec *exec, struct xe_vma *vma,
-				 bool res_evict, bool validate)
+				 bool validate)
 {
 	struct xe_bo *bo = xe_vma_bo(vma);
+	struct xe_vm *vm = xe_vma_vm(vma);
 	int err = 0;
 
 	if (bo) {
 		if (!bo->vm)
 			err = drm_exec_lock_obj(exec, &bo->ttm.base);
 		if (!err && validate)
-			err = xe_bo_validate(bo, xe_vma_vm(vma),
-					     !xe_vm_in_preempt_fence_mode(xe_vma_vm(vma)) &&
-					     res_evict);
+			err = xe_bo_validate(bo, vm,
+					     !xe_vm_in_preempt_fence_mode(vm));
 	}
 
 	return err;
@@ -2459,21 +2459,13 @@ static int check_ufence(struct xe_vma *vma)
 }
 
 static int op_lock_and_prep(struct drm_exec *exec, struct xe_vm *vm,
-			    struct xe_vma_ops *vops, struct xe_vma_op *op)
+			    struct xe_vma_op *op)
 {
 	int err = 0;
-	bool res_evict;
-
-	/*
-	 * We only allow evicting a BO within the VM if it is not part of an
-	 * array of binds, as an array of binds can evict another BO within the
-	 * bind.
-	 */
-	res_evict = !(vops->flags & XE_VMA_OPS_ARRAY_OF_BINDS);
 
 	switch (op->base.op) {
 	case DRM_GPUVA_OP_MAP:
-		err = vma_lock_and_validate(exec, op->map.vma, res_evict,
+		err = vma_lock_and_validate(exec, op->map.vma,
 					    !xe_vm_in_fault_mode(vm) ||
 					    op->map.immediate);
 		break;
@@ -2484,13 +2476,11 @@ static int op_lock_and_prep(struct drm_exec *exec, struct xe_vm *vm,
 
 		err = vma_lock_and_validate(exec,
 					    gpuva_to_vma(op->base.remap.unmap->va),
-					    res_evict, false);
+					    false);
 		if (!err && op->remap.prev)
-			err = vma_lock_and_validate(exec, op->remap.prev,
-						    res_evict, true);
+			err = vma_lock_and_validate(exec, op->remap.prev, true);
 		if (!err && op->remap.next)
-			err = vma_lock_and_validate(exec, op->remap.next,
-						    res_evict, true);
+			err = vma_lock_and_validate(exec, op->remap.next, true);
 		break;
 	case DRM_GPUVA_OP_UNMAP:
 		err = check_ufence(gpuva_to_vma(op->base.unmap.va));
@@ -2499,7 +2489,7 @@ static int op_lock_and_prep(struct drm_exec *exec, struct xe_vm *vm,
 
 		err = vma_lock_and_validate(exec,
 					    gpuva_to_vma(op->base.unmap.va),
-					    res_evict, false);
+					    false);
 		break;
 	case DRM_GPUVA_OP_PREFETCH:
 	{
@@ -2510,7 +2500,7 @@ static int op_lock_and_prep(struct drm_exec *exec, struct xe_vm *vm,
 
 		err = vma_lock_and_validate(exec,
 					    gpuva_to_vma(op->base.prefetch.va),
-					    res_evict, false);
+					    false);
 		if (!err && !xe_vma_has_no_bo(vma))
 			err = xe_bo_migrate(xe_vma_bo(vma),
 					    region_to_mem_type[region]);
@@ -2535,7 +2525,7 @@ static int vm_bind_ioctl_ops_lock_and_prep(struct drm_exec *exec,
 		return err;
 
 	list_for_each_entry(op, &vops->list, link) {
-		err = op_lock_and_prep(exec, vm, vops, op);
+		err = op_lock_and_prep(exec, vm, op);
 		if (err)
 			return err;
 	}
@@ -3136,8 +3126,6 @@ int xe_vm_bind_ioctl(struct drm_device *dev, void *data, struct drm_file *file)
 	}
 
 	xe_vma_ops_init(&vops, vm, q, syncs, num_syncs);
-	if (args->num_binds > 1)
-		vops.flags |= XE_VMA_OPS_ARRAY_OF_BINDS;
 	for (i = 0; i < args->num_binds; ++i) {
 		u64 range = bind_ops[i].range;
 		u64 addr = bind_ops[i].addr;
