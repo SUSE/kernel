6122dd6994581eb186ceccdc08891a09d552f5e0
Patrik Jakobsson <pjakobsson@suse.de>
2026-02-25 17:01:57 +0100

    drm/amdgpu/atom: Work around vbios NULL offset false positive (jsc#PED-13979 jsc#PED-14039 jsc#PED-14046 jsc#PED-14211 jsc#PED-14333 jsc#PED-14487 jsc#PED-14488 jsc#PED-14497 jsc#PED-14499 jsc#PED-15868)
    suse-commit: 6c21626bb8d5d7bb11867c66d6fed4bbfc3cac2c

--- drivers/gpu/drm/amd/amdgpu/atom.c
+++ drivers/gpu/drm/amd/amdgpu/atom.c
@@ -1448,6 +1448,7 @@ static void atom_get_vbios_pn(struct atom_context *ctx)
 		if (vbios_str == NULL)
 			vbios_str += sizeof(BIOS_ATOM_PREFIX) - 1;
 	}
+	OPTIMIZER_HIDE_VAR(vbios_str);
 	if (vbios_str != NULL && *vbios_str == 0)
 		vbios_str++;
 
