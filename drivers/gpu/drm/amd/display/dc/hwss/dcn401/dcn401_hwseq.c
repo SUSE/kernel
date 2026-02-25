e4537de6d95edb0999ad11232cca785e3fb50690
Patrik Jakobsson <pjakobsson@suse.de>
2026-02-25 17:04:18 +0100

    Revert "drm/amd/display: Refactor SubVP cursor limiting logic" (jsc#PED-13979 jsc#PED-14039 jsc#PED-14046 jsc#PED-14211 jsc#PED-14333 jsc#PED-14487 jsc#PED-14488 jsc#PED-14497 jsc#PED-14499 jsc#PED-15868)
    suse-commit: ba2631c60ef9f3b81a9faf20ba565b6f7a695ad0

--- drivers/gpu/drm/amd/display/dc/hwss/dcn401/dcn401_hwseq.c
+++ drivers/gpu/drm/amd/display/dc/hwss/dcn401/dcn401_hwseq.c
@@ -2656,7 +2656,7 @@ bool dcn401_update_bandwidth(
 	struct dce_hwseq *hws = dc->hwseq;
 
 	/* recalculate DML parameters */
-	if (dc->res_pool->funcs->validate_bandwidth(dc, context, false) != DC_OK)
+	if (!dc->res_pool->funcs->validate_bandwidth(dc, context, false))
 		return false;
 
 	/* apply updated bandwidth parameters */
