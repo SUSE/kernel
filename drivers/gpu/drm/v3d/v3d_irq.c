e98c37da74503f66dc6dda8536e5ce7e0d80f61a
Patrik Jakobsson <pjakobsson@suse.de>
2026-02-25 16:11:01 +0100

    drm/v3d: Associate a V3D tech revision to all supported devices (jsc#PED-13979 jsc#PED-14039 jsc#PED-14046 jsc#PED-14211 jsc#PED-14333 jsc#PED-14487 jsc#PED-14488 jsc#PED-14497 jsc#PED-14499 jsc#PED-15868)
    suse-commit: b4d31d414f5164d7b17492cb4545933779bc3773

--- drivers/gpu/drm/v3d/v3d_irq.c
+++ drivers/gpu/drm/v3d/v3d_irq.c
@@ -143,7 +143,7 @@ v3d_irq(int irq, void *arg)
 	/* We shouldn't be triggering these if we have GMP in
 	 * always-allowed mode.
 	 */
-	if (v3d->ver < 71 && (intsts & V3D_INT_GMPV))
+	if (v3d->ver < V3D_GEN_71 && (intsts & V3D_INT_GMPV))
 		dev_err(v3d->drm.dev, "GMP violation\n");
 
 	/* V3D 4.2 wires the hub and core IRQs together, so if we &
@@ -200,7 +200,7 @@ v3d_hub_irq(int irq, void *arg)
 
 		V3D_WRITE(V3D_MMU_CTL, V3D_READ(V3D_MMU_CTL));
 
-		if (v3d->ver >= 41) {
+		if (v3d->ver >= V3D_GEN_41) {
 			axi_id = axi_id >> 5;
 			if (axi_id < ARRAY_SIZE(v3d41_axi_ids))
 				client = v3d41_axi_ids[axi_id];
@@ -217,7 +217,7 @@ v3d_hub_irq(int irq, void *arg)
 		status = IRQ_HANDLED;
 	}
 
-	if (v3d->ver >= 71 && (intsts & V3D_V7_HUB_INT_GMPV)) {
+	if (v3d->ver >= V3D_GEN_71 && (intsts & V3D_V7_HUB_INT_GMPV)) {
 		dev_err(v3d->drm.dev, "GMP Violation\n");
 		status = IRQ_HANDLED;
 	}
@@ -228,7 +228,7 @@ v3d_hub_irq(int irq, void *arg)
 int
 v3d_irq_init(struct v3d_dev *v3d)
 {
-	int irq, ret, core;
+	int irq1, ret, core;
 
 	INIT_WORK(&v3d->overflow_mem_work, v3d_overflow_mem_work);
 
@@ -239,24 +239,17 @@ v3d_irq_init(struct v3d_dev *v3d)
 		V3D_CORE_WRITE(core, V3D_CTL_INT_CLR, V3D_CORE_IRQS(v3d->ver));
 	V3D_WRITE(V3D_HUB_INT_CLR, V3D_HUB_IRQS(v3d->ver));
 
-	irq = platform_get_irq_optional(v3d_to_pdev(v3d), 1);
-	if (irq == -EPROBE_DEFER)
-		return irq;
-	if (irq > 0) {
-		v3d->irq[V3D_CORE_IRQ] = irq;
-
-		ret = devm_request_irq(v3d->drm.dev, v3d->irq[V3D_CORE_IRQ],
+	irq1 = platform_get_irq_optional(v3d_to_pdev(v3d), 1);
+	if (irq1 == -EPROBE_DEFER)
+		return irq1;
+	if (irq1 > 0) {
+		ret = devm_request_irq(v3d->drm.dev, irq1,
 				       v3d_irq, IRQF_SHARED,
 				       "v3d_core0", v3d);
 		if (ret)
 			goto fail;
-
-		irq = platform_get_irq(v3d_to_pdev(v3d), 0);
-		if (irq < 0)
-			return irq;
-		v3d->irq[V3D_HUB_IRQ] = irq;
-
-		ret = devm_request_irq(v3d->drm.dev, v3d->irq[V3D_HUB_IRQ],
+		ret = devm_request_irq(v3d->drm.dev,
+				       platform_get_irq(v3d_to_pdev(v3d), 0),
 				       v3d_hub_irq, IRQF_SHARED,
 				       "v3d_hub", v3d);
 		if (ret)
@@ -264,12 +257,8 @@ v3d_irq_init(struct v3d_dev *v3d)
 	} else {
 		v3d->single_irq_line = true;
 
-		irq = platform_get_irq(v3d_to_pdev(v3d), 0);
-		if (irq < 0)
-			return irq;
-		v3d->irq[V3D_CORE_IRQ] = irq;
-
-		ret = devm_request_irq(v3d->drm.dev, v3d->irq[V3D_CORE_IRQ],
+		ret = devm_request_irq(v3d->drm.dev,
+				       platform_get_irq(v3d_to_pdev(v3d), 0),
 				       v3d_irq, IRQF_SHARED,
 				       "v3d", v3d);
 		if (ret)
@@ -309,12 +298,6 @@ v3d_irq_disable(struct v3d_dev *v3d)
 	for (core = 0; core < v3d->cores; core++)
 		V3D_CORE_WRITE(core, V3D_CTL_INT_MSK_SET, ~0);
 	V3D_WRITE(V3D_HUB_INT_MSK_SET, ~0);
-
-	/* Finish any interrupt handler still in flight. */
-	for (int i = 0; i < V3D_MAX_IRQS; i++) {
-		if (v3d->irq[i])
-			synchronize_irq(v3d->irq[i]);
-	}
 
 	/* Clear any pending interrupts we might have left. */
 	for (core = 0; core < v3d->cores; core++)
